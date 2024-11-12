33a463aa1042b8d7ddc933ac70259208e2c91f4e
Gabriel Vasconcelos <127629717+gvrodrigues2@users.noreply.github.com>
2024-11-11 22:38:34 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <Router basename="/tela-login-DIO">
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/feed" element={<Feed />} />
